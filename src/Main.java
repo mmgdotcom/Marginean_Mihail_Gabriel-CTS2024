183ba5f52e57ded4a665c6fc774482aa8c5587fa
Mihai Marginean <mihail_gabriel_marginean@yahoo.com>
2025-03-18 23:41:24 +0200

    Fourth commit

--- src/Main.java
+++ src/Main.java
@@ -288,5 +288,170 @@ public class Main {
                 break;
         }
 
+        //Statement 1 is executed (one time) before the execution of the code block.
+        //Statement 2 defines the condition for executing the code block.
+        //Statement 3 is executed (every time) after the code block has been executed.
+        //The example below will print the numbers 0 to 4:
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+        //Statement 1 sets a variable before the loop starts (int i = 0).
+        //Statement 2 defines the condition for the loop to run (i must be less than 5).
+        //If the condition is true, the loop will start over again, if it is false, the loop will end.
+        //Statement 3 increases a value (i++) each time the code block in the loop has been executed.
+
+        //nested loops
+        //outer loop
+        for (int i = 1; i <= 2; i++) {
+            System.out.println("Outer: " + i); //executes 2 times
+
+            //inner loop
+            for (int j = 1; j <= 3; j++) {
+                System.out.println(" Inner: " + j); //executes 6 times (2 * 3)
+            }
+        }
+
+        //for-each loop
+        /*
+        for (type variableName : arrayName) {
+            //code block to be executed
+        }
+        */
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
+        //program that counts to 100 by tens
+        for (int i = 0; i <= 100; i += 10){
+            System.out.println(i);
+        }
+
+        //print the multiplication table for a specified number
+        int number = 2;
+         for (int i = 1; i <= 10; i++) {
+             System.out.println(number + " x " + i + " = " + (number * i));
+         }
+
+         //example to stop the loop when i is equal to 4
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                break;
+            }
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+        //Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+        //To declare an array, define the variable type with square brackets:
+        //String[] cars;
+        String[] cars1 = {"Volvo", "BMW", "Ford"};
+        //int[] myNum = {10, 20, 30};
+        //You can access an array element by referring to the index number.
+        System.out.println("The first car from the cars1 variable is: " + cars1[0] + ".");
+        //Note: Array indexes start with 0: [0] is the first element. [1] is the second element, etc.
+        //To change the value of a specific element, refer to the index number:
+        cars1[0] = "Opel";
+        System.out.println("The first car from the cars1 variable is updated to: " + cars1[0] + ".");
+        //To find out how many elements an array has, use the length property:
+        System.out.println("The cars1 variable length is: " + cars1.length); //prints the number of elements
+
+        for (int i = 0; i < cars1.length; i++) {
+            System.out.println("The car no. " + (i + 1) + " is " + cars1[i]);
+        }
+
+        for (String i : cars1) {
+            System.out.println(i);
+        }
+        //The example above can be read like this: for each String element (called i - as in index) in cars,
+        //print out the value of i.
+        //If you compare the for loop and for-each loop, you will see that the for-each method is easier to write,
+        //it does not require a counter (using the length property), and it is more readable.
+
+        //practical example of using arrays - program that calculates the average of different ages
+        //an array storing different ages
+        int[] ages = {20, 22, 18, 35, 48, 26, 87, 70};
+
+        float avg, sum = 0;
+
+        //get the length of the array
+        int lengthAges = ages.length;
+
+        //loop through the elements of the array
+        for(int age : ages) {
+            sum += age; //For each age in ages, add age to sum.
+        }
+
+        //calculate the average by dividing the sum by the length
+        avg = sum / lengthAges;
+
+        //print the average
+        System.out.println("The average age is: " + avg + ".");
+
+        //program that finds the lowest age among different ages
+        //nn array storing different ages
+        int ages1[] = {20, 22, 18, 35, 48, 26, 87, 70};
+
+        //get the length of the array
+        int length1 = ages.length;
+
+        //create a 'lowest age' variable and assign the first array element of ages to it
+        int lowestAge = ages[0];
+
+        //loop through the elements of the ages array to find the lowest age
+        for (int age : ages) {
+            //check if the current age is smaller than the current 'lowest age'
+            if (lowestAge > age) {
+                //if the smaller age is found, update 'lowest age' with that element
+                lowestAge = age;
+            }
+        }
+
+        //output the value of the lowest age
+        System.out.println("The lowest age in the array is: " + lowestAge);
+
+        //Multidimensional Arrays
+        //A multidimensional array is an array of arrays.
+        //Multidimensional arrays are useful when you want to store data as a tabular form,
+        //like a table with rows and columns.
+        //To create a two-dimensional array, add each array within its own set of curly braces:
+        //int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+        //myNumbers is now an array with two arrays as its elements.
+        //To access the elements of the myNumbers array, specify two indexes: one for the array,
+        //and one for the element inside that array.
+        //This example accesses the third element (2) in the second array (1) of myNumbers:
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+        System.out.println(myNumbers[1][2]); //outputs 7
+        System.out.println(myNumbers[0][1]); //outputs 2
+        //Remember that: Array indexes start with 0: [0] is the first element. [1] is the second element, etc.
+
+        //You can also change the value of an element:
+        myNumbers[1][2] = 9;
+        System.out.println(myNumbers[1][2]); // Outputs 9 instead of 7
+        System.out.println("---------");
+
+        //You can also use a for loop inside another for loop to get the elements of
+        //a two-dimensional array (we still have to point to the two indexes):
+        for (int i = 0; i < myNumbers.length; ++i) {
+            for (int j = 0; j < myNumbers[i].length; ++j) {
+                System.out.println(myNumbers[i][j]);
+            }
+        }
+
+        System.out.println("---------");
+
+        //Or you could just use a for-each loop, which is considered easier to read and write:
+        for (int[] row : myNumbers) {
+            for (int i : row) {
+                System.out.println(i);
+            }
+        }
     }
 }
