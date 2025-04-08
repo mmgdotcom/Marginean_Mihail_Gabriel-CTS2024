897b298286079fd2e343a926de01e4e9c7b1f701
Mihai Marginean <mihail_gabriel_marginean@yahoo.com>
2025-04-09 00:21:08 +0300

    18 commit

---
+++ src/intalnire_1_8_martie_3/Nota.java
@@ -0,0 +1,42 @@
+package intalnire_1_8_martie_3;
+
+public class Nota {
+    String denumire;
+    int valoare;
+
+    public Nota() {
+        this.denumire = "";
+        this.valoare = 0;
+    }
+
+    public Nota(String newDenumire) {
+        this.denumire = newDenumire;
+        this.valoare = 0;
+    }
+
+    public Nota(String newDenumire, int newValoare) {
+        this.denumire = newDenumire;
+        this.valoare = newValoare;
+    }
+
+    public String getDenumire() {
+        return this.denumire;
+    }
+
+    public int getValoare() {
+        return this.valoare;
+    }
+
+    void setDenumire(String newDenumire) {
+        this.denumire = newDenumire;
+    }
+
+    void setValoare(int newValoare) {
+        this.valoare = newValoare;
+    }
+
+    @Override public String toString () {
+        return "denumire: " + denumire + "valoare: " + valoare;
+    }
+
+}
---
+++ src/intalnire_1_8_martie_3/Student.java
@@ -0,0 +1,9 @@
+package intalnire_1_8_martie_3;
+
+import java.util.ArrayList;
+
+public class Student {
+    int cod;
+    String nume;
+    ArrayList<Nota> note;
+}
